ece3448c364cec00435535755312333b7dc05904
Romain Baumier <baumier.romain@gmail.com>
2014-12-16 13:31:12 +0100

    Remove PolluterStraight => straight is now default behavior so RobotPolluter = ex-PolluterStraight

--- RobotPolluter.java
+++ RobotPolluter.java
@@ -1,10 +1,17 @@
 public class RobotPolluter extends Robot {
+  public int startCol;
 
-  public RobotPolluter(int num, int x, int y, World world) {
+  public RobotPolluter(int num, int x, int y, World world, int startCol) {
     super(num, x, y, world);
+    startCol = startCol; // WTF ? diff between x and startCol ??
   }
 
   public void pollutes() {
     world.board[x][y] = -1;
+  }
+
+  public void roam() {
+    //TODO : go straight and pollutes every case
+    //startPoint in (0, startCol)
   }
 }
