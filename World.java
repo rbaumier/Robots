d8bb1275ebe7fdcef68c4c8d97b5b58d45b47ca8
Romain Baumier <baumier.romain@gmail.com>
2014-12-17 09:30:42 +0100

    Add show() and repeat()

--- World.java
+++ World.java
@@ -1,15 +1,18 @@
+
 public class World {
-  private int widht;
+  private int width;
   private int length;
   private int[][] board;
 
-  public World(int length, int widht) {
+  public World(int length, int width) {
+    this.width = width;
+    this.length = length;
     board = setDefaultValues(new int[width][length]);
   }
 
   public int[][] setDefaultValues(int[][] board) {
-    for (int i = 0; i < width; i++) {
-      for (int j = 0; j < length; j++) {
+    for (int i = 0; i < this.width; i++) {
+      for (int j = 0; j < this.length; j++) {
         board[i][j] = 0;
       }
     }
@@ -24,9 +27,27 @@ public class World {
     board[x][y] = id;
   }
 
-  public String toString() {
-    //case X : dirty, "." clean
+  public String repeat(String toRepeat, int times) {
+    String str = "";
+    for (int i = 0; i < times; i++) {
+      str += toRepeat;
+    }
+    return str;
   }
+
+  public void show() {
+    System.out.println(repeat("---", this.width) + "-");
+    for (int i = 0; i < this.width; ++i) {
+      System.out.print("|");
+      for (int j = 0; j < this.length; ++j) {
+        System.out.print(" " + board[i][j] + " ");
+      }
+      System.out.print("|");
+      System.out.println("");
+    }
+    System.out.println(repeat("---", this.width) + "-");
+  }
+
 
   public void pollute(int x, int y) {
     board[x][y] = -1;
@@ -38,5 +59,10 @@ public class World {
 
   public boolean isDirty(int x, int y) {
     return board[x][y] == -1;
+  }
+
+  public static void main(String[] args) {
+    World board = new World(10,10);
+    board.show();
   }
 }
