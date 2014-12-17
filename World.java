335a0211b55d20f632d3f37f5b88a4886b3dd98c
Romain Baumier <baumier.romain@gmail.com>
2014-12-17 10:53:18 +0100

    move cells methods from world to cell

--- World.java
+++ World.java
@@ -2,6 +2,7 @@ public class World {
   private int width;
   private int height;
   private int[][] board;
+  private List<Robot> robots = new ArrayList<Robot>();
 
   public World(int width, int height) {
     if(hasCorrectSize(width, height)) {
@@ -14,25 +15,16 @@ public class World {
   }
 
   public static boolean hasCorrectSize(int width, int height) {
-    System.out.println("width:" + width + " height: " + height);
     return width <= 30 && height <= 20;
   }
 
   public int[][] setDefaultValues(int[][] board) {
     for (int i = 0; i < this.height; i++) {
       for (int j = 0; j < this.width; j++) {
-        board[i][j] = 0;
+        board[i][j] = new Cell(i,j);
       }
     }
     return board;
-  }
-
-  public boolean isFree(int x, int y) {
-    return board[x][y] <= 0;
-  }
-
-  public void use(int x, int y, int id) {
-    board[x][y] = id;
   }
 
   public String repeat(String toRepeat, int times) {
@@ -54,18 +46,6 @@ public class World {
       System.out.println("");
     }
     System.out.println(repeat("---", this.width) + "-");
-  }
-
-  public void pollute(int x, int y) {
-    board[x][y] = -1;
-  }
-
-  public void clean(int x, int y) {
-    board[x][y] = 0;
-  }
-
-  public boolean isDirty(int x, int y) {
-    return board[x][y] == -1;
   }
 
   public static void main(String[] args) {
