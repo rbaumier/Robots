9e2728b9ece5f100ee17fc8188869ac3d23c71d2
Romain Baumier <baumier.romain@gmail.com>
2014-12-18 11:03:41 +0100

    Add boustrophedon movement for cleaners

--- RobotCleaner.java
+++ RobotCleaner.java
@@ -8,7 +8,36 @@ public class RobotCleaner extends Robot {
     world.board[x][y].clean();
   }
 
-  public void roam() {
-    //TODO : boustrophedon mode with start at (0,0) and clean every case
+  public void roam(Cell[][] board) {
+    //boustrophedonFromTop(board);
+    //boustrophedonFromBottom(board);
+  }
+
+  public void boustrophedonFromTop(Cell[][] board) {
+    for (var i = 0; i < board.length; i++) {
+      if (i%2 == 0) {
+        for (var j = 0; j < board.length; j++) {
+          // System.out.println(board[i][j].show());
+        }
+      } else {
+        for (var k = board.length-1; k >= 0; k--) {
+          // System.out.println(board[i][k].show());
+        }
+      }
+    }
+  }
+
+  public void boustrophedonFromBottom(Cell[][] board) {
+    for (var i = board.length-1; i >= 0; i--) {
+      if (i%2 !== 0) {
+        for (var j = 0; j < board.length; j++) {
+          // System.out.println(board[i][j].show());
+        }
+      } else {
+        for (var k = board.length-1; k >= 0; k--) {
+          // System.out.println(board[i][k].show());
+        }
+      }
+    }
   }
 }
