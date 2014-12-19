c6081605071eca59dcaaa61f0fed0df71f096aa0
Romain Baumier <baumier.romain@gmail.com>
2014-12-19 13:50:00 +0100

    PolluterJumper random get xn, yn

--- PolluterJumper.java
+++ PolluterJumper.java
@@ -1,12 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class PolluterJumper extends RobotPolluter {
   public PolluterJumper(int num, int x, int y, World world) {
     super(num, x, y, world);
   }
 
   public void move() {
+    List<Integer> pos = new ArrayList<Integer>();
+    pos.add(-2);
+    pos.add(-1);
+    pos.add(1);
+    pos.add(2);
 
+    Random random = new Random();
+
+    int xn = x+pos.get(random.nextInt(3 - 0 + 1) + 0);
+    int yn = x+pos.get(random.nextInt(3 - 0 + 1) + 0);
+
+    System.out.println(xn);
+    System.out.println(yn);
     // if(isFree(xn, yn)) {
     //   setPosition(xn, yn, this);
+    // } else {
+    //   move();
     // }
   }
 }
