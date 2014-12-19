65b92ccea73fd25286787caea0e08da112b02bd2
Romain Baumier <baumier.romain@gmail.com>
2014-12-19 09:37:06 +0100

    Remove thread + sleep for each robot

--- Robot.java
+++ Robot.java
@@ -11,17 +11,5 @@ public abstract class Robot extends Thread{
     this.world = world;
   }
 
-  public void run() {
-    roam(500);
-  }
-
-  public void sleep(int sleepTime) {
-    try {
-      Thread.sleep(sleepTime);
-    } catch(InterruptedException ex) {
-      Thread.currentThread().interrupt();
-    }
-  }
-
-  public void roam(int sleepTime) {}
+  public void move() {}
 }
--- World.java
+++ World.java
@@ -42,35 +42,20 @@ public class World extends Thread {
     this.board[x][y].addRobot(robot);
   }
 
-  public void enable(int sleepTime) {
-    for(Robot r : this.robots) {
-      r.roam(sleepTime);
-    }
-  }
-
   public static void main(String[] args) {
     World world = new World(7,4);
 
     RobotPolluter robotPolluter = new RobotPolluter(1, 0, 0,  world);
-    robotPolluter.start();
-
     PolluterJumper polluterJumper = new PolluterJumper(2, 0, world.width-1, world);
-    polluterJumper.start();
-
     RobotCleaner robotCleaner = new RobotCleaner(3, world.height-1, 0, world);
-    robotCleaner.start();
-
     CleanerDistract cleanerDistract = new CleanerDistract(4, world.height-1, world.width-1, world);
-    cleanerDistract.start();
 
-    Screen screen = new Screen(world);
-    screen.start();
+    Timer screen = new Timer(world);
 
     world.robots.add(robotPolluter);
     world.robots.add(polluterJumper);
     world.robots.add(robotCleaner);
     world.robots.add(cleanerDistract);
-    world.enable(Integer.parseInt(args[0]));
 
     System.out.println("plop");
   }
