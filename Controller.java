51ab08a31f2116d048921ef88d6958f09c024d4b
Romain Baumier <baumier.romain@gmail.com>
2014-12-19 11:29:51 +0100

    Add view in controller

--- Controller.java
+++ Controller.java
@@ -1,15 +1,18 @@
 public class Controller {
   public World world;
+  public View view;
 
   public Controller() {}
 
-  public Controller(World w) {
+  public Controller(World w, View v) {
     world = w;
+    view = v;
   }
 
   public void update() {
     for(Robot r : world.robots) {
       r.move();
     }
+    view.refresh();
   }
 }
