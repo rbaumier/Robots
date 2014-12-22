e5220cf4435ecc7af522c9aa0209849191cbb3e0
Romain Baumier <baumier.romain@gmail.com>
2014-12-22 12:32:26 +0100

    Draw robots moving, but the panel is opaque

--- BoardPanel.java
+++ BoardPanel.java
@@ -1,8 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 
 public class BoardPanel extends JPanel {
   public World world;
--- RobotPanel.java
+++ RobotPanel.java
@@ -1,5 +1,6 @@
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -8,6 +9,10 @@ import javax.swing.JPanel;
 public class RobotPanel extends JPanel {
   public World world;
 
+  public RobotPanel(World w) {
+    world = w;
+  }
+
   @Override
   public void paintComponent(Graphics g){
     super.paintComponent(g);
@@ -15,11 +20,19 @@ public class RobotPanel extends JPanel {
   }
 
   public void drawRobots(Graphics g) {
-    try {
-      Image img = ImageIO.read(new File("img/clean.png"));
-      g.drawImage(img, 0, 0, this);
-    } catch (IOException e) {
-      e.printStackTrace();
+    for (int i = 0; i < world.height; i++) {
+      for (int j = 0; j < world.width; j++) {
+
+        if(!world.board[i][j].isFree()) {
+          Robot robot = world.board[i][j].robot;
+          try {
+            Image img = ImageIO.read(new File("img/clean.png"));
+            g.drawImage(img, robot.x*100, robot.y*100, this);
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+      }
     }
   }
 }
