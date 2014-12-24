35b30e24d925468b70d41180fd7519c58e156318
Romain Baumier <baumier.romain@gmail.com>
2014-12-24 09:57:21 +0100

    PolluterJumper.move()

--- src/org/robots/models/PolluterJumper.java
+++ src/org/robots/models/PolluterJumper.java
@@ -5,34 +5,37 @@ import java.util.List;
 import java.util.Random;
 
 public class PolluterJumper extends RobotPolluter {
-  List<Integer> pos;
+  public int[][] pos = {
+    { -1, 1 }, {-2, 2}
+  };
+
 
   public PolluterJumper(int num, int x, int y, World world) {
     super(num, x, y, world);
-    pos = new ArrayList<Integer>();
-    pos.add(-2);
-    pos.add(-1);
-    pos.add(1);
-    pos.add(2);
   }
 
   public void release() {
     world.board[y][x].release();
   }
 
-  // public void move() {
-  //   Random random = new Random();
+  public void move() {
+    int rAx = new Random().nextInt((1 - 0) + 1);
+    int rBx = new Random().nextInt((1 - 0) + 1);
 
-  //   int xn = x+pos.get(random.nextInt(3 - 0 + 1) + 0);
-  //   int yn = x+pos.get(random.nextInt(3 - 0 + 1) + 0);
+    int rAy = rAx == 0 ? 1 : 0;
+    int rBy = rAx == 0 ? 1 : 0;
 
-  //   if(world.isValidCell(xn, yn) && world.board[xn][yn].isFree()) {
-  //     x = xn;
-  //     y = yn;
-  //     world.board[x][y].pollute();
-  //     world.board[x][y].use(this);
-  //   } else {
-  //     move();
-  //   }
-  // }
+    int xn = x + pos[rAx][rBx];
+    int yn = y + pos[rAy][rBy];
+
+    if(world.isValidCell(xn, yn)) {
+      x = xn;
+      y = yn;
+      use();
+      pollute();
+    } else {
+      move();
+    }
+
+  }
 }
